5777dab33c59776a4d9044f6e7dd37a74bd7c613
Rafael Vera <rafaeldejesusverareyes@gmail.com>
2022-05-25 17:07:40 -0500

    Update app.py
    Added the show_img function that displays the image without saving it to a file.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import base64
+from PIL import Image
+import io
 from flask import Flask, jsonify, request
 
 # Se instancia el servidor y se almacena
@@ -22,16 +24,23 @@ def predict():
     if image_b64 is None:
         return jsonify({'message': 'No image'})
     # Se decodifica la imagen (queda en binario)
-    save_img(image_b64)
+    image_b = base64.b64decode(image_b64)
+    show_img(image_b)
+    #save_img(image_b)
     # Se envia a una funcion para predecir
     # Se muestra la imagen condificada en base64
     return f'<img src="data:image/png;base64,{image_b64}">'
 
+# Funcion que muestra la imagen recibida en binario
+def show_img(image_b: bytes) -> None:
+    image = Image.open(io.BytesIO(image_b))
+    image.show()
+
 # Funcion que recibe la imagen en base64
 # y la almacena en la carpeta img
-def save_img(image_b64) -> None:
+def save_img(image_b: bytes) -> None:
     with open('img/image.jpg', 'wb') as file:
-        file.write(base64.b64decode(image_b64))
+        file.write(image_b)
 
 # Inicio de la ejecucion
 if __name__ == '__main__':

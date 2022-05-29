136297f24424a10c6c8c41c0ec6917051b50160d
Rafael Vera <rafaeldejesusverareyes@gmail.com>
2022-05-29 02:06:52 -0500

    Create file_paths.py

---
+++ file_paths.py
@@ -0,0 +1,5 @@
+# Paths where the models are stored
+PATH_SHALLOW_MODEL_ADADELTA = 'models/shallow_model_adadelta.h5'
+PATH_DEEP_MODEL_ADADELTA    = 'models/deep_model_adadelta.h5'
+PATH_SHALLOW_MODEL_SGD      = 'models/shallow_model_sgd.h5'
+PATH_DEEP_MODEL_SGD         = 'models/deep_model_sgd.h5'
